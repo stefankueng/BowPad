1b5e746f77c9c20545ee9f41362e9f8bd1725ddc
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2013-05-09 06:57:28 +0000

    When doing Copy/Cut, also put the selection in "Html Format" on the clipboard, styled and colored according to the view.

--- src/Commands/CmdClipboard.h
+++ src/Commands/CmdClipboard.h
@@ -18,12 +18,157 @@
 #pragma once
 #include "ICommand.h"
 #include "BowPadUI.h"
+#include "StringUtils.h"
+#include "UnicodeUtils.h"
 
-class CCmdCut : public ICommand
+
+class ClipboardBase : public ICommand
+{
+public:
+    ClipboardBase(void * obj) : ICommand(obj)
+    {
+    }
+
+    virtual ~ClipboardBase(void)
+    {
+    }
+
+protected:
+    std::string GetHtmlSelection()
+    {
+        std::string sHtmlFragment;
+        int style = 0;
+
+        char fontbuf[40] = {0};
+        ScintillaCall(SCI_STYLEGETFONT, 0, (sptr_t)fontbuf);
+        int fontSize = (int)ScintillaCall(SCI_STYLEGETSIZE, 0);
+        bool bold = !!ScintillaCall(SCI_STYLEGETBOLD, 0);
+        bool italic = !!ScintillaCall(SCI_STYLEGETITALIC, 0);
+        bool underlined = !!ScintillaCall(SCI_STYLEGETUNDERLINE, 0);
+        COLORREF fore = (COLORREF)ScintillaCall(SCI_STYLEGETFORE, 0);
+        COLORREF back = (COLORREF)ScintillaCall(SCI_STYLEGETBACK, 0);
+        std::string stylehtml = CStringUtils::Format("<pre style=\"font-family:%s;font-size:%dpt;font-weight:%s;font-style:%s;text-decoration:%s;color:#%06x;background:#%06x;\">",
+            fontbuf, fontSize, bold ? "bold" : "normal", italic ? "italic" : "normal", underlined ? "underline" : "none",
+            GetRValue(fore)<<16 | GetGValue(fore)<<8 | GetBValue(fore),
+            GetRValue(back)<<16 | GetGValue(back)<<8 | GetBValue(back));
+        sHtmlFragment += stylehtml;
+
+
+        int numSelections = (int)ScintillaCall(SCI_GETSELECTIONS);
+        bool spanset = false;
+        for (int i = 0; i < numSelections; ++i)
+        {
+            int selStart = (int)ScintillaCall(SCI_GETSELECTIONNSTART, i);
+            int selEnd   = (int)ScintillaCall(SCI_GETSELECTIONNEND, i);
+
+            if ((selStart == selEnd) && (numSelections == 1))
+            {
+                int curLine = (int)ScintillaCall(SCI_LINEFROMPOSITION, ScintillaCall(SCI_GETCURRENTPOS));
+                selStart = (int)ScintillaCall(SCI_POSITIONFROMLINE, curLine);
+                selEnd = (int)ScintillaCall(SCI_GETLINEENDPOSITION, curLine);
+            }
+
+            int p = selStart;
+            do 
+            {
+                int s = (int)ScintillaCall(SCI_GETSTYLEAT, p);
+                if (s != style)
+                {
+                    if (spanset)
+                        sHtmlFragment += "</span>";
+                    ScintillaCall(SCI_STYLEGETFONT, s, (sptr_t)fontbuf);
+                    fontSize = (int)ScintillaCall(SCI_STYLEGETSIZE, s);
+                    bold = !!ScintillaCall(SCI_STYLEGETBOLD, s);
+                    italic = !!ScintillaCall(SCI_STYLEGETITALIC, s);
+                    underlined = !!ScintillaCall(SCI_STYLEGETUNDERLINE, s);
+                    fore = (COLORREF)ScintillaCall(SCI_STYLEGETFORE, s);
+                    back = (COLORREF)ScintillaCall(SCI_STYLEGETBACK, s);
+                    stylehtml = CStringUtils::Format("<span style=\"font-family:%s;font-size:%dpt;font-weight:%s;font-style:%s;text-decoration:%s;color:#%06x;background:#%06x;\">",
+                        fontbuf, fontSize, bold ? "bold" : "normal", italic ? "italic" : "normal", underlined ? "underline" : "none",
+                        GetRValue(fore)<<16 | GetGValue(fore)<<8 | GetBValue(fore),
+                        GetRValue(back)<<16 | GetGValue(back)<<8 | GetBValue(back));
+                    sHtmlFragment += stylehtml;
+                    style = s;
+                    spanset = true;
+                }
+                char c = (char)ScintillaCall(SCI_GETCHARAT, p);
+                std::string cs;
+                cs += c;
+                switch (c)
+                {
+                case ' ':
+                    cs = "&nbsp;";
+                    break;
+                case '"':
+                    cs = "&quot;";
+                    break;
+                case '<':
+                    cs = "&lt;";
+                    break;
+                case '>':
+                    cs = "&gt;";
+                    break;
+                }
+                sHtmlFragment += cs;
+                ++p;
+            } while (p < selEnd);
+            sHtmlFragment += "\r\n";
+        }
+        sHtmlFragment += "</span></pre>";
+
+        std::string header = "Version:0.9\r\nStartHTML:<<<<<<<1\r\nEndHTML:<<<<<<<2\r\nStartFragment:<<<<<<<3\r\nEndFragment:<<<<<<<4\r\nStartSelection:<<<<<<<3\r\nEndSelection:<<<<<<<3\r\n";
+        std::string pre = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\r\n<HTML><HEAD><TITLE>BowPad</TITLE></HEAD>\r\n<BODY>\r\n<!--StartFragment-->";
+        std::string post = "<!--EndFragment--></BODY></HTML>";
+
+        std::string sHtml = header;
+        int startHtml = (int)sHtml.length();
+        sHtml += pre;
+        int startFragment = (int)sHtml.length();
+        sHtml += sHtmlFragment;
+        int endFragment = (int)sHtml.length();
+        sHtml += post;
+        int endHtml = (int)sHtml.length();
+
+        // replace back offsets
+        SearchReplace(sHtml, "<<<<<<<1", CStringUtils::Format("%08d", startHtml));
+        SearchReplace(sHtml, "<<<<<<<2", CStringUtils::Format("%08d", endHtml));
+        SearchReplace(sHtml, "<<<<<<<3", CStringUtils::Format("%08d", startFragment));
+        SearchReplace(sHtml, "<<<<<<<4", CStringUtils::Format("%08d", endFragment));
+
+        return sHtml;
+    }
+    void AddHtmlStringToClipboard(const std::string& sHtml)
+    {
+        if (OpenClipboard(GetHwnd()))
+        {
+            HGLOBAL hClipboardData;
+            size_t sLen = sHtml.length();
+            hClipboardData = GlobalAlloc(GMEM_DDESHARE, (sLen+1)*sizeof(char));
+            if (hClipboardData)
+            {
+                char * pchData;
+                pchData = (char*)GlobalLock(hClipboardData);
+                if (pchData)
+                {
+                    strcpy_s(pchData, sLen+1, sHtml.c_str());
+                    if (GlobalUnlock(hClipboardData))
+                    {
+                        UINT CF_HTML = RegisterClipboardFormat(L"HTML Format");
+                        SetClipboardData(CF_HTML, hClipboardData);
+                    }
+                }
+            }
+            CloseClipboard();
+        }
+    }
+};
+
+
+class CCmdCut : public ClipboardBase
 {
 public:
 
-    CCmdCut(void * obj) : ICommand(obj)
+    CCmdCut(void * obj) : ClipboardBase(obj)
     {
     }
 
@@ -33,7 +178,9 @@ public:
 
     virtual bool Execute()
     {
+        std::string sHtml = GetHtmlSelection();
         ScintillaCall(SCI_CUT);
+        AddHtmlStringToClipboard(sHtml);
         return true;
     }
 
@@ -57,11 +204,11 @@ public:
 };
 
 
-class CCmdCopy : public ICommand
+class CCmdCopy : public ClipboardBase
 {
 public:
 
-    CCmdCopy(void * obj) : ICommand(obj)
+    CCmdCopy(void * obj) : ClipboardBase(obj)
     {
     }
 
@@ -71,7 +218,9 @@ public:
 
     virtual bool Execute()
     {
+        std::string sHtml = GetHtmlSelection();
         ScintillaCall(SCI_COPYALLOWLINE);
+        AddHtmlStringToClipboard(sHtml);
         return true;
     }
 
