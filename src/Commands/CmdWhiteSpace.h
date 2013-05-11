7f49ab1b290eb96768a33115665a7578fecc7319
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2013-05-11 14:15:51 +0000

    Add commands to set the tab width and use of spaces/tabs.

--- src/Commands/CmdWhiteSpace.h
+++ src/Commands/CmdWhiteSpace.h
@@ -54,3 +54,109 @@ public:
     }
 
 };
+
+class CCmdTabSize : public ICommand
+{
+public:
+
+    CCmdTabSize(void * obj) : ICommand(obj)
+    {
+        int ve = (int)CIniSettings::Instance().GetInt64(L"View", L"tabsize", 4);
+        ScintillaCall(SCI_SETTABWIDTH, ve);
+        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_DecimalValue);
+    }
+
+    ~CCmdTabSize(void)
+    {
+    }
+
+    virtual bool Execute() { return true; }
+
+    virtual UINT GetCmdId() { return cmdTabSize; }
+
+    virtual HRESULT IUICommandHandlerUpdateProperty( REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue )
+    {
+        HRESULT hr = S_OK;
+        // Set the minimum value
+        if (IsEqualPropertyKey(key, UI_PKEY_MinValue))
+        {
+            ZeroMemory(ppropvarNewValue, sizeof(*ppropvarNewValue));
+            ppropvarNewValue->vt = VT_DECIMAL;
+            VarDecFromR8(1, &ppropvarNewValue->decVal);
+            hr = S_OK;
+        }
+        // Set the maximum value
+        else if (IsEqualPropertyKey(key, UI_PKEY_MaxValue))
+        {
+            ZeroMemory(ppropvarNewValue, sizeof(*ppropvarNewValue));
+            ppropvarNewValue->vt = VT_DECIMAL;
+            VarDecFromR8(20, &ppropvarNewValue->decVal);
+            hr = S_OK;
+        }
+        // Set the increment
+        else if (IsEqualPropertyKey(key, UI_PKEY_Increment))
+        {
+            ZeroMemory(ppropvarNewValue, sizeof(*ppropvarNewValue));
+            ppropvarNewValue->vt = VT_DECIMAL;
+            VarDecFromR8(1, &ppropvarNewValue->decVal);
+            hr = S_OK;
+        }
+        // Set the number of decimal places
+        else if (IsEqualPropertyKey(key, UI_PKEY_DecimalPlaces))
+        {
+            hr = InitPropVariantFromUInt32(0, ppropvarNewValue);
+        }
+        // Set the initial value
+        else if (IsEqualPropertyKey(key, UI_PKEY_DecimalValue))
+        {
+            int ve = (int)CIniSettings::Instance().GetInt64(L"View", L"tabsize", 4);
+            ZeroMemory(ppropvarNewValue, sizeof(*ppropvarNewValue));
+            ppropvarNewValue->vt = VT_DECIMAL;
+            VarDecFromR8(ve, &ppropvarNewValue->decVal);
+        }
+        return hr;
+    }
+
+    virtual HRESULT IUICommandHandlerExecute( UI_EXECUTIONVERB /*verb*/, const PROPERTYKEY* /*key*/, const PROPVARIANT* ppropvarValue, IUISimplePropertySet* /*pCommandExecutionProperties*/ )
+    {
+        ScintillaCall(SCI_SETTABWIDTH, ppropvarValue->intVal);
+        CIniSettings::Instance().SetInt64(L"View", L"tabsize", ppropvarValue->intVal);
+        return S_OK;
+    }
+};
+
+class CCmdUseTabs : public ICommand
+{
+public:
+
+    CCmdUseTabs(void * obj) : ICommand(obj)
+    {
+        int ws = (int)CIniSettings::Instance().GetInt64(L"View", L"usetabs", 1);
+        ScintillaCall(SCI_SETUSETABS, ws);
+        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_BooleanValue);
+    }
+
+    ~CCmdUseTabs(void)
+    {
+    }
+
+    virtual bool Execute()
+    {
+        ScintillaCall(SCI_SETUSETABS, ScintillaCall(SCI_GETUSETABS) ? 0 : 1);
+        CIniSettings::Instance().SetInt64(L"View", L"usetabs", ScintillaCall(SCI_GETUSETABS));
+        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_BooleanValue);
+        return true;
+    }
+
+    virtual UINT GetCmdId() { return cmdUseTabs; }
+
+    virtual HRESULT IUICommandHandlerUpdateProperty( REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue )
+    {
+        if (UI_PKEY_BooleanValue == key)
+        {
+            return UIInitPropertyFromBoolean(UI_PKEY_BooleanValue, ScintillaCall(SCI_GETUSETABS) > 0, ppropvarNewValue);
+        }
+        return E_NOTIMPL;
+    }
+
+};
