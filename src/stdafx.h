83830725b7b6c0cae7f87d2bd69f9accce0c8644
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2013-05-25 15:47:31 +0000

    Set the AppID.

--- src/stdafx.h
+++ src/stdafx.h
@@ -44,3 +44,5 @@
 #pragma comment(lib, "shlwapi.lib")
 
 #pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#define APP_ID L"TortoiseSVN.Tools.BowPad.1"
