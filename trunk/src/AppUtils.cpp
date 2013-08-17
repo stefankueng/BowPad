28743312fa08da080c369f2382ed8d4b880e9492
steveking <steveking@d94fd7d9-36d9-44dd-b333-5229c786babd>
2013-08-17 12:36:14 +0000

    * To extend the limit of only 15 items selected at once to show the "Edit in BowPad" context menu to show up, set the "MultiSelectModel" entry to "Player". That extends the limit to 100 selected files. * Use the %APPDATA% folder in case BowPad is installed and not running as a portable app

--- trunk/src/AppUtils.cpp
+++ trunk/src/AppUtils.cpp
@@ -144,18 +144,46 @@ CAppUtils::~CAppUtils(void)
 
 std::wstring CAppUtils::GetDataPath(HMODULE hMod)
 {
-    DWORD len = 0;
-    DWORD bufferlen = MAX_PATH;     // MAX_PATH is not the limit here!
-    std::unique_ptr<wchar_t[]> path(new wchar_t[bufferlen]);
-    do
+    static std::wstring datapath;
+    if (datapath.empty())
     {
-        bufferlen += MAX_PATH;      // MAX_PATH is not the limit here!
-        path = std::unique_ptr<wchar_t[]>(new wchar_t[bufferlen]);
-        len = GetModuleFileName(hMod, path.get(), bufferlen);
-    } while(len == bufferlen);
-    std::wstring sPath = path.get();
-    sPath = sPath.substr(0, sPath.find_last_of('\\'));
-    return sPath;
+        // in case BowPad was installed with the installer, there's a registry
+        // entry made by the installer.
+        HKEY subKey = nullptr;
+        LONG result = RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"Software\\BowPad", 0, KEY_READ, &subKey);
+        if (result != ERROR_SUCCESS)
+        {
+            // BowPad was not installed, which means it's run as a portable app:
+            // use the same directory as the exe is in to store the application data
+            DWORD len = 0;
+            DWORD bufferlen = MAX_PATH;     // MAX_PATH is not the limit here!
+            std::unique_ptr<wchar_t[]> path(new wchar_t[bufferlen]);
+            do
+            {
+                bufferlen += MAX_PATH;      // MAX_PATH is not the limit here!
+                path = std::unique_ptr<wchar_t[]>(new wchar_t[bufferlen]);
+                len = GetModuleFileName(hMod, path.get(), bufferlen);
+            } while(len == bufferlen);
+            std::wstring sPath = path.get();
+            sPath = sPath.substr(0, sPath.find_last_of('\\'));
+            datapath = CPathUtils::GetLongPathname(sPath);
+        }
+        else
+        {
+            RegCloseKey(subKey);
+            // BowPad is installed: we must not store the application data
+            // in the same directory as the exe is but in %APPDATA%\BowPad instead
+            PWSTR outpath = nullptr;
+            if (SUCCEEDED(SHGetKnownFolderPath(FOLDERID_RoamingAppData, KF_FLAG_CREATE, NULL, &outpath)))
+            {
+                datapath = outpath;
+                datapath += L"\\BowPad";
+                datapath = CPathUtils::GetLongPathname(datapath);
+                CoTaskMemFree(outpath);
+            }
+        }
+    }
+    return datapath;
 }
 
 std::wstring CAppUtils::GetSessionID()
