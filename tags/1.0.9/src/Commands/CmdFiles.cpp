95deb1ce29f3e8e23534f495e5df32f4e1a81599
steveking <steveking@d94fd7d9-36d9-44dd-b333-5229c786babd>
2014-01-19 16:59:57 +0000

    Tag the 1.0.9 release

---
+++ tags/1.0.9/src/Commands/CmdFiles.cpp
@@ -0,0 +1,194 @@
+// This file is part of BowPad.
+//
+// Copyright (C) 2013-2014 - Stefan Kueng
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// See <http://www.gnu.org/licenses/> for a copy of the full license text
+//
+#include "stdafx.h"
+#include "CmdFiles.h"
+#include "MRU.h"
+#include "LexStyles.h"
+#include "PreserveChdir.h"
+
+bool CCmdOpen::Execute()
+{
+    PreserveChdir keepCWD;
+
+    std::vector<std::wstring> paths;
+    CComPtr<IFileOpenDialog> pfd = NULL;
+
+    HRESULT hr = pfd.CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER);
+    if (SUCCEEDED(hr))
+    {
+        // Set the dialog options
+        DWORD dwOptions;
+        if (SUCCEEDED(hr = pfd->GetOptions(&dwOptions)))
+        {
+            hr = pfd->SetOptions(dwOptions | FOS_FILEMUSTEXIST | FOS_FORCEFILESYSTEM | FOS_PATHMUSTEXIST | FOS_ALLOWMULTISELECT);
+        }
+
+        // Set a title
+        if (SUCCEEDED(hr))
+        {
+            pfd->SetTitle(L"BowPad");
+        }
+
+        // Show the save/open file dialog
+        if (SUCCEEDED(hr) && SUCCEEDED(hr = pfd->Show(GetHwnd())))
+        {
+            CComPtr<IShellItemArray> psiaResults;
+            hr = pfd->GetResults(&psiaResults);
+            if (SUCCEEDED(hr))
+            {
+                DWORD count = 0;
+                hr = psiaResults->GetCount(&count);
+                for (DWORD i = 0; i < count; ++i)
+                {
+                    CComPtr<IShellItem> psiResult = NULL;
+                    hr = psiaResults->GetItemAt(i, &psiResult);
+                    PWSTR pszPath = NULL;
+                    hr = psiResult->GetDisplayName(SIGDN_FILESYSPATH, &pszPath);
+                    if (SUCCEEDED(hr))
+                    {
+                        paths.push_back(pszPath);
+                    }
+                }
+            }
+        }
+    }
+    if (SUCCEEDED(hr))
+    {
+        for (auto it : paths)
+            OpenFile(it.c_str());
+    }
+    return true;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+bool CCmdSave::Execute()
+{
+    return SaveCurrentTab();
+}
+
+void CCmdSave::ScintillaNotify( Scintilla::SCNotification * pScn )
+{
+    switch (pScn->nmhdr.code)
+    {
+    case SCN_SAVEPOINTREACHED:
+    case SCN_SAVEPOINTLEFT:
+        {
+            InvalidateUICommand(UI_INVALIDATIONS_STATE, NULL);
+        }
+        break;
+    }
+}
+
+void CCmdSave::TabNotify(TBHDR * ptbhdr)
+{
+    if (ptbhdr->hdr.code == TCN_SELCHANGE)
+    {
+        InvalidateUICommand(UI_INVALIDATIONS_STATE, NULL);
+    }
+}
+
+HRESULT CCmdSave::IUICommandHandlerUpdateProperty( REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue )
+{
+    if (UI_PKEY_Enabled == key)
+    {
+        if (HasActiveDocument())
+        {
+            CDocument doc = GetActiveDocument();
+            return UIInitPropertyFromBoolean(UI_PKEY_Enabled, doc.m_bIsDirty||doc.m_bNeedsSaving, ppropvarNewValue);
+        }
+    }
+    return E_NOTIMPL;
+}
+
+bool CCmdSaveAll::Execute()
+{
+    for (int i = 0; i < (int)GetDocumentCount(); ++i)
+    {
+        if (GetDocumentFromID(GetDocIDFromTabIndex(i)).m_bIsDirty)
+        {
+            TabActivateAt(i);
+            SaveCurrentTab();
+        }
+    }
+    return true;
+}
+
+void CCmdSaveAll::ScintillaNotify( Scintilla::SCNotification * pScn )
+{
+    switch (pScn->nmhdr.code)
+    {
+    case SCN_SAVEPOINTREACHED:
+    case SCN_SAVEPOINTLEFT:
+        {
+            InvalidateUICommand(UI_INVALIDATIONS_STATE, NULL);
+        }
+        break;
+    }
+}
+
+void CCmdSaveAll::TabNotify(TBHDR * ptbhdr)
+{
+    if (ptbhdr->hdr.code == TCN_SELCHANGE)
+    {
+        InvalidateUICommand(UI_INVALIDATIONS_STATE, NULL);
+    }
+}
+
+HRESULT CCmdSaveAll::IUICommandHandlerUpdateProperty( REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue )
+{
+    if (UI_PKEY_Enabled == key)
+    {
+        int dirtycount = 0;
+        for (int i = 0; i < GetDocumentCount(); ++i)
+        {
+            CDocument doc = GetDocumentFromID(GetDocIDFromTabIndex(i));
+            if (doc.m_bIsDirty||doc.m_bNeedsSaving)
+                dirtycount++;
+        }
+
+        return UIInitPropertyFromBoolean(UI_PKEY_Enabled, dirtycount>0, ppropvarNewValue);
+    }
+    return E_NOTIMPL;
+}
+
+
+bool CCmdSaveAs::Execute()
+{
+    return SaveCurrentTab(true);
+}
+
+HRESULT CCmdReload::IUICommandHandlerUpdateProperty( REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue )
+{
+    if (UI_PKEY_Enabled == key)
+    {
+        if (HasActiveDocument())
+        {
+            CDocument doc = GetActiveDocument();
+            return UIInitPropertyFromBoolean(UI_PKEY_Enabled, !doc.m_path.empty(), ppropvarNewValue);
+        }
+    }
+    return E_NOTIMPL;
+}
+
+void CCmdReload::TabNotify( TBHDR * ptbhdr )
+{
+    if (ptbhdr->hdr.code == TCN_SELCHANGE)
+    {
+        InvalidateUICommand(UI_INVALIDATIONS_STATE, NULL);
+    }
+}
---
+++ tags/1.0.9/src/Commands/CmdLaunch.h
@@ -0,0 +1,180 @@
+// This file is part of BowPad.
+//
+// Copyright (C) 2013-2014 - Stefan Kueng
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// See <http://www.gnu.org/licenses/> for a copy of the full license text
+//
+
+#pragma once
+#include "ICommand.h"
+#include "BowPadUI.h"
+#include "StringUtils.h"
+#include "PathUtils.h"
+#include "UnicodeUtils.h"
+#include "EscapeUtils.h"
+#include "BaseDialog.h"
+#include "DlgResizer.h"
+
+class LaunchBase : public ICommand
+{
+public:
+    LaunchBase(void * obj) : ICommand(obj){}
+    virtual ~LaunchBase(void){}
+
+protected:
+    bool Launch(const std::wstring& cmdline);
+};
+
+class CCmdLaunchIE : public LaunchBase
+{
+public:
+    CCmdLaunchIE(void * obj) : LaunchBase(obj){}
+    ~CCmdLaunchIE(void){}
+
+    virtual bool Execute() override { return Launch(L"iexplore \"$(TAB_PATH)\""); }
+    virtual UINT GetCmdId() override { return cmdLaunchIE; }
+};
+
+class CCmdLaunchFirefox : public LaunchBase
+{
+public:
+    CCmdLaunchFirefox(void * obj) : LaunchBase(obj){}
+    ~CCmdLaunchFirefox(void){}
+
+    virtual bool Execute() override { return Launch(L"firefox \"$(TAB_PATH)\""); }
+    virtual UINT GetCmdId() override { return cmdLaunchFirefox; }
+};
+
+class CCmdLaunchChrome : public LaunchBase
+{
+public:
+    CCmdLaunchChrome(void * obj) : LaunchBase(obj){}
+    ~CCmdLaunchChrome(void){}
+
+    virtual bool Execute() override { return Launch(L"chrome \"$(TAB_PATH)\""); }
+    virtual UINT GetCmdId() override { return cmdLaunchChrome; }
+};
+
+class CCmdLaunchSafari : public LaunchBase
+{
+public:
+    CCmdLaunchSafari(void * obj) : LaunchBase(obj){}
+    ~CCmdLaunchSafari(void){}
+
+    virtual bool Execute() override { return Launch(L"safari \"$(TAB_PATH)\""); }
+    virtual UINT GetCmdId() override { return cmdLaunchSafari; }
+};
+
+class CCmdLaunchOpera : public LaunchBase
+{
+public:
+    CCmdLaunchOpera(void * obj) : LaunchBase(obj){}
+    ~CCmdLaunchOpera(void){}
+
+    virtual bool Execute() override { return Launch(L"opera \"$(TAB_PATH)\""); }
+    virtual UINT GetCmdId() override { return cmdLaunchOpera; }
+};
+
+class CCmdLaunchSearch : public LaunchBase
+{
+public:
+    CCmdLaunchSearch(void * obj) : LaunchBase(obj){}
+    ~CCmdLaunchSearch(void){}
+
+    virtual bool Execute() override
+    {
+        std::wstring sLaunch = CIniSettings::Instance().GetString(L"CustomLaunch", L"websearch", L"http://www.google.com/search?q=$(SEL_TEXT_ESCAPED)");
+        if (sLaunch.empty())
+            sLaunch = L"http://www.google.com/search?q=$(SEL_TEXT_ESCAPED)";
+        return Launch(sLaunch);
+    }
+    virtual UINT GetCmdId() override { return cmdLaunchSearch; }
+};
+
+class CCmdLaunchWikipedia : public LaunchBase
+{
+public:
+    CCmdLaunchWikipedia(void * obj) : LaunchBase(obj){}
+    ~CCmdLaunchWikipedia(void){}
+
+    virtual bool Execute() override { return Launch(L"http://en.wikipedia.org/wiki/Special:Search?search=$(SEL_TEXT_ESCAPED)"); }
+    virtual UINT GetCmdId() override { return cmdLaunchWikipedia; }
+};
+
+class CCmdLaunchConsole : public LaunchBase
+{
+public:
+    CCmdLaunchConsole(void * obj) : LaunchBase(obj){}
+    ~CCmdLaunchConsole(void){}
+
+    virtual bool Execute() override { return Launch(L"cmd /K cd /d \"$(TAB_DIR)\""); }
+    virtual UINT GetCmdId() override { return cmdLaunchConsole; }
+};
+
+class CCmdLaunchExplorer : public LaunchBase
+{
+public:
+    CCmdLaunchExplorer(void * obj) : LaunchBase(obj){}
+    ~CCmdLaunchExplorer(void){}
+
+    virtual bool Execute() override { return Launch(L"explorer \"$(TAB_DIR)\""); }
+    virtual UINT GetCmdId() override { return cmdLaunchExplorer; }
+};
+
+class CCmdLaunchCustom : public LaunchBase
+{
+public:
+    CCmdLaunchCustom(UINT customId, void * obj) : m_customId(customId), LaunchBase(obj)
+    {
+        m_customCmdId = cmdLaunchCustom0 + customId;
+        m_settingsID = CStringUtils::Format(L"Command%d", customId);
+        InvalidateUICommand(m_customCmdId, UI_INVALIDATIONS_PROPERTY, &UI_PKEY_Label);
+        InvalidateUICommand(m_customCmdId, UI_INVALIDATIONS_STATE, &UI_PKEY_Enabled);
+    }
+    ~CCmdLaunchCustom(void){}
+
+    virtual bool Execute() override { return Launch(CIniSettings::Instance().GetString(L"CustomLaunch", m_settingsID.c_str(), L"")); }
+    virtual UINT GetCmdId() override { return m_customCmdId; }
+
+    virtual HRESULT IUICommandHandlerUpdateProperty(REFPROPERTYKEY key, const PROPVARIANT* ppropvarCurrentValue, PROPVARIANT* ppropvarNewValue) override;
+
+private:
+    UINT            m_customId;
+    UINT            m_customCmdId;
+    std::wstring    m_settingsID;
+};
+
+class CCustomCommandsDlg : public CDialog
+{
+public:
+    CCustomCommandsDlg();
+    ~CCustomCommandsDlg(void);
+
+protected:
+    LRESULT CALLBACK        DlgFunc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+    LRESULT                 DoCommand(int id);
+
+private:
+    CDlgResizer             m_resizer;
+};
+
+class CCmdCustomCommands : public ICommand
+{
+public:
+    CCmdCustomCommands(void * obj) : ICommand(obj) {}
+    ~CCmdCustomCommands(void){}
+
+    virtual bool Execute() override;
+    virtual UINT GetCmdId() override { return cmdCustomCommands; }
+};
+
