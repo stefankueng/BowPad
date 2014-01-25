e26d62e8c5dca1c1c6d2ef848c9baec4d113254a
steveking <steveking@d94fd7d9-36d9-44dd-b333-5229c786babd>
2014-01-25 14:19:21 +0000

    Replace \n with a space instead of \r

--- trunk/src/Commands/CmdFunctions.cpp
+++ trunk/src/Commands/CmdFunctions.cpp
@@ -1,6 +1,6 @@
 // This file is part of BowPad.
 //
-// Copyright (C) 2013 - Stefan Kueng
+// Copyright (C) 2013-2014 - Stefan Kueng
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -317,8 +317,8 @@ void CCmdFunctions::FindFunctions(int docID, bool bBackground)
                     m_ScratchScintilla.Call(SCI_GETTEXTRANGE, 0, (sptr_t)&funcrange);
                     size_t line = m_ScratchScintilla.Call(SCI_LINEFROMPOSITION, funcrange.chrg.cpMin);
                     std::wstring sf = CUnicodeUtils::StdGetUnicode(strbuf.get());
-                    SearchReplace(sf, L"\r", L" ");
-                    SearchReplace(sf, L"\n", L"");
+                    SearchReplace(sf, L"\r", L"");
+                    SearchReplace(sf, L"\n", L" ");
                     SearchReplace(sf, L"{", L"");
                     SearchReplace(sf, L"\t", L" ");
                     if (!trimtokens.empty())
