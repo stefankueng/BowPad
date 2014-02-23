ff3c69ccd29094b8d1ac4949af2134de35868f19
steveking <steveking@d94fd7d9-36d9-44dd-b333-5229c786babd>
2014-02-23 15:32:20 +0000

    Create the 1.1.2 branch

---
+++ branches/1.1.2/src/Commands/CmdCodeStyle.cpp
@@ -0,0 +1,180 @@
+// This file is part of BowPad.
+//
+// Copyright (C) 2013-2014 - Stefan Kueng
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// See <http://www.gnu.org/licenses/> for a copy of the full license text
+//
+#include "stdafx.h"
+#include "CmdCodeStyle.h"
+#include "PropertySet.h"
+#include "BowPad.h"
+#include "StringUtils.h"
+#include "LexStyles.h"
+
+#include <vector>
+#include <tuple>
+
+std::vector<std::wstring> langs;
+
+HRESULT CCmdCodeStyle::IUICommandHandlerUpdateProperty( REFPROPERTYKEY key, const PROPVARIANT* ppropvarCurrentValue, PROPVARIANT* ppropvarNewValue )
+{
+    HRESULT hr = E_FAIL;
+
+    if(key == UI_PKEY_Categories)
+    {
+        IUICollection* pCollection;
+        hr = ppropvarCurrentValue->punkVal->QueryInterface(IID_PPV_ARGS(&pCollection));
+        if (FAILED(hr))
+        {
+            return hr;
+        }
+
+        if (langs.empty())
+            langs = CLexStyles::Instance().GetLanguages();
+
+        for (wchar_t i = 'A'; i <= 'Z'; ++i)
+        {
+            // Create a property set for the category.
+            CPropertySet *pCat;
+            hr = CPropertySet::CreateInstance(&pCat);
+            if (FAILED(hr))
+            {
+                pCollection->Release();
+                return hr;
+            }
+
+            wchar_t sName[2] = {0};
+            sName[0] = i;
+            // Initialize the property set with the label that was just loaded and a category id of 0.
+            pCat->InitializeCategoryProperties(sName, i-'A');
+            pCollection->Add(pCat);
+            pCat->Release();
+        }
+    }
+    else if (key == UI_PKEY_ItemsSource)
+    {
+        IUICollection* pCollection;
+        hr = ppropvarCurrentValue->punkVal->QueryInterface(IID_PPV_ARGS(&pCollection));
+        if (FAILED(hr))
+        {
+            return hr;
+        }
+
+        if (langs.empty())
+            langs = CLexStyles::Instance().GetLanguages();
+
+        // Create an IUIImage from a resource id.
+        IUIImage * pImg = nullptr;
+        IUIImageFromBitmap * pifbFactory = nullptr;
+        hr = CoCreateInstance(CLSID_UIRibbonImageFromBitmapFactory, NULL, CLSCTX_ALL, IID_PPV_ARGS(&pifbFactory));
+        if (FAILED(hr))
+        {
+            return hr;
+        }
+
+        // Load the bitmap from the resource file.
+        HBITMAP hbm = (HBITMAP) LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_EMPTY), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
+        if (hbm)
+        {
+            // Use the factory implemented by the framework to produce an IUIImage.
+            hr = pifbFactory->CreateImage(hbm, UI_OWNERSHIP_TRANSFER, &pImg);
+            if (FAILED(hr))
+            {
+                DeleteObject(hbm);
+            }
+        }
+        pifbFactory->Release();
+
+        if (FAILED(hr))
+        {
+            pCollection->Release();
+            return hr;
+        }
+
+        // populate the dropdown with the code pages
+        for (auto it:langs)
+        {
+            // Create a new property set for each item.
+            CPropertySet* pItem;
+            hr = CPropertySet::CreateInstance(&pItem);
+            if (FAILED(hr))
+            {
+                pCollection->Release();
+                return hr;
+            }
+
+            int catId = it.c_str()[0] - 'A';
+            pItem->InitializeItemProperties(pImg, it.c_str(), catId);
+
+            // Add the newly-created property set to the collection supplied by the framework.
+            pCollection->Add(pItem);
+
+            pItem->Release();
+        }
+        pImg->Release();
+        pCollection->Release();
+        hr = S_OK;
+    }
+    else if (key == UI_PKEY_SelectedItem)
+    {
+        if (HasActiveDocument())
+        {
+            CDocument doc = GetActiveDocument();
+            hr = S_FALSE;
+            for (size_t i = 0; i < langs.size(); ++i)
+            {
+                if (langs[i] == doc.m_language)
+                {
+                    hr = UIInitPropertyFromUInt32(UI_PKEY_SelectedItem, (UINT)i, ppropvarNewValue);
+                    break;
+                }
+            }
+        }
+    }
+    return hr;
+}
+
+HRESULT CCmdCodeStyle::IUICommandHandlerExecute( UI_EXECUTIONVERB verb, const PROPERTYKEY* key, const PROPVARIANT* ppropvarValue, IUISimplePropertySet* /*pCommandExecutionProperties*/ )
+{
+    HRESULT hr = E_FAIL;
+
+    if (verb == UI_EXECUTIONVERB_EXECUTE)
+    {
+        if ( key && *key == UI_PKEY_SelectedItem)
+        {
+            UINT selected;
+            hr = UIPropertyToUInt32(*key, *ppropvarValue, &selected);
+            if (HasActiveDocument())
+            {
+                CDocument doc = GetActiveDocument();
+                doc.m_language = langs[selected];
+                SetDocument(GetCurrentTabId(), doc);
+                SetupLexerForLang(doc.m_language);
+                CLexStyles::Instance().SetLangForPath(doc.m_path, doc.m_language);
+                UpdateStatusBar(true);
+            }
+            hr = S_OK;
+        }
+    }
+    return hr;
+}
+
+void CCmdCodeStyle::TabNotify( TBHDR * ptbhdr )
+{
+    if (ptbhdr->hdr.code == TCN_SELCHANGE)
+    {
+        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_Enabled);
+        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_SelectedItem);
+    }
+}
+
---
+++ branches/1.1.2/src/Commands/CmdMisc.h
@@ -0,0 +1,236 @@
+// This file is part of BowPad.
+//
+// Copyright (C) 2013-2014 - Stefan Kueng
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// See <http://www.gnu.org/licenses/> for a copy of the full license text
+//
+
+#pragma once
+#include "ICommand.h"
+#include "BowPad.h"
+#include "BowPadUI.h"
+#include "AppUtils.h"
+#include "Theme.h"
+#include "SysInfo.h"
+
+class CCmdDelete : public ICommand
+{
+public:
+
+    CCmdDelete(void * obj) : ICommand(obj)
+    {
+    }
+
+    ~CCmdDelete(void)
+    {
+    }
+
+    virtual bool Execute() override
+    {
+        ScintillaCall(SCI_CLEAR);
+        return true;
+    }
+
+    virtual UINT GetCmdId() override { return cmdDelete; }
+};
+
+
+class CCmdSelectAll : public ICommand
+{
+public:
+
+    CCmdSelectAll(void * obj) : ICommand(obj)
+    {
+    }
+
+    ~CCmdSelectAll(void)
+    {
+    }
+
+    virtual bool Execute() override
+    {
+        ScintillaCall(SCI_SELECTALL);
+        return true;
+    }
+
+    virtual UINT GetCmdId() override { return cmdSelectAll; }
+};
+
+class CCmdGotoBrace : public ICommand
+{
+public:
+
+    CCmdGotoBrace(void * obj) : ICommand(obj)
+    {
+    }
+
+    ~CCmdGotoBrace(void)
+    {
+    }
+
+    virtual bool Execute() override
+    {
+        GotoBrace();
+        return true;
+    }
+
+    virtual UINT GetCmdId() override { return cmdGotoBrace; }
+};
+
+class CCmdToggleTheme : public ICommand
+{
+public:
+
+    CCmdToggleTheme(void * obj) : ICommand(obj)
+    {
+        CTheme::Instance().GetRibbonColors(text, back, high);
+        int dark = (int)CIniSettings::Instance().GetInt64(L"View", L"darktheme", 0);
+        if (dark)
+        {
+            CTheme::Instance().SetDarkTheme(!CTheme::Instance().IsDarkTheme());
+            if (IsWindows8OrGreater())
+                CTheme::Instance().SetRibbonColorsHSB(UI_HSB(0,0,255), UI_HSB(160,0,0), UI_HSB(160,44,0));
+            else
+                CTheme::Instance().SetRibbonColors(RGB(255,255,255), RGB(20,20,20), RGB(153,76,0));
+        }
+        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_BooleanValue);
+    }
+
+    ~CCmdToggleTheme(void)
+    {
+    }
+
+    virtual bool Execute() override
+    {
+        if (!HasActiveDocument())
+            return false;
+        CTheme::Instance().SetDarkTheme(!CTheme::Instance().IsDarkTheme());
+        CDocument doc = GetActiveDocument();
+        SetupLexerForLang(doc.m_language);
+        if (CTheme::Instance().IsDarkTheme())
+        {
+            if (IsWindows8OrGreater())
+                CTheme::Instance().SetRibbonColorsHSB(UI_HSB(255,255,0), UI_HSB(160,44,0), UI_HSB(160,44,0));
+            else
+                CTheme::Instance().SetRibbonColors(RGB(255,255,255), RGB(20,20,20), RGB(153,76,0));
+        }
+        else
+            CTheme::Instance().SetRibbonColorsHSB(text, back, high);
+
+        CIniSettings::Instance().SetInt64(L"View", L"darktheme", CTheme::Instance().IsDarkTheme() ? 1 : 0);
+
+        ScintillaCall(SCI_CLEARDOCUMENTSTYLE);
+        ScintillaCall(SCI_COLOURISE, 0, -1);
+        TabActivateAt(GetActiveTabIndex());
+
+        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_BooleanValue);
+        return true;
+    }
+
+    virtual UINT GetCmdId() override { return cmdToggleTheme; }
+
+    virtual HRESULT IUICommandHandlerUpdateProperty(REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue) override
+    {
+        if (UI_PKEY_BooleanValue == key)
+        {
+            return UIInitPropertyFromBoolean(UI_PKEY_BooleanValue, CTheme::Instance().IsDarkTheme(), ppropvarNewValue);
+        }
+        return E_NOTIMPL;
+    }
+private:
+    UI_HSBCOLOR text;
+    UI_HSBCOLOR back;
+    UI_HSBCOLOR high;
+};
+
+class CCmdConfigShortcuts : public ICommand
+{
+public:
+
+    CCmdConfigShortcuts(void * obj) : ICommand(obj)
+    {
+    }
+
+    ~CCmdConfigShortcuts(void)
+    {
+    }
+
+    virtual bool Execute() override
+    {
+        std::wstring userFile = CAppUtils::GetDataPath() + L"\\shortcuts.ini";
+        if (!PathFileExists(userFile.c_str()))
+        {
+            HRSRC hRes = FindResource(NULL, MAKEINTRESOURCE(IDR_SHORTCUTS), L"config");
+            if (hRes)
+            {
+                HGLOBAL hResourceLoaded = LoadResource(NULL, hRes);
+                if (hResourceLoaded)
+                {
+                    char * lpResLock = (char *) LockResource(hResourceLoaded);
+                    if (lpResLock)
+                    {
+                        char * lpStart = strstr(lpResLock, "#--");
+                        if (lpStart)
+                        {
+                            char * lpEnd = strstr(lpStart + 3, "#--");
+                            if (lpEnd)
+                            {
+                                HANDLE hFile = CreateFile(userFile.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+                                if (hFile != INVALID_HANDLE_VALUE)
+                                {
+                                    DWORD dwWritten = 0;
+                                    WriteFile(hFile, lpStart, (DWORD)(lpEnd-lpStart), &dwWritten, NULL);
+                                    CloseHandle(hFile);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return OpenFile(userFile.c_str(), false);
+    }
+
+    virtual UINT GetCmdId() override { return cmdConfigShortcuts; }
+};
+
+class CCmdAutoBraces : public ICommand
+{
+public:
+
+    CCmdAutoBraces(void * obj) : ICommand(obj)
+    {
+        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_BooleanValue);
+    }
+
+    ~CCmdAutoBraces(void)
+    {}
+
+    virtual bool Execute() override
+    {
+        CIniSettings::Instance().SetInt64(L"View", L"autobrace", CIniSettings::Instance().GetInt64(L"View", L"autobrace", 1) ? 0 : 1);
+        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_BooleanValue);
+        return true;
+    }
+
+    virtual UINT GetCmdId() override { return cmdAutoBraces; }
+
+    virtual HRESULT IUICommandHandlerUpdateProperty(REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue) override
+    {
+        if (UI_PKEY_BooleanValue == key)
+        {
+            return UIInitPropertyFromBoolean(UI_PKEY_BooleanValue, (BOOL)CIniSettings::Instance().GetInt64(L"View", L"autobrace", 1), ppropvarNewValue);
+        }
+        return E_NOTIMPL;
+    }
+};
