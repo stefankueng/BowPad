76b16c8dca00d351e6a491479b7dc0eb711b51bf
steveking <steveking@d94fd7d9-36d9-44dd-b333-5229c786babd>
2014-06-01 14:25:02 +0000

    Create the 2.0.0 branch

---
+++ branches/2.0.0/src/Commands/CmdPlugins.h
@@ -0,0 +1,45 @@
+// This file is part of BowPad.
+//
+// Copyright (C) 2014 - Stefan Kueng
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// See <http://www.gnu.org/licenses/> for a copy of the full license text
+//
+
+#pragma once
+#include "ICommand.h"
+#include "BowPadUI.h"
+
+#include <string>
+#include <vector>
+
+
+class CCmdPlugins : public ICommand
+{
+public:
+
+    CCmdPlugins(void * obj) : ICommand(obj)
+    {
+    }
+
+    ~CCmdPlugins(void)
+    {
+    }
+
+    virtual bool Execute() override { return false; }
+    virtual UINT GetCmdId() override { return cmdPlugins; }
+
+
+    virtual HRESULT IUICommandHandlerUpdateProperty(REFPROPERTYKEY key, const PROPVARIANT* ppropvarCurrentValue, PROPVARIANT* ppropvarNewValue) override;
+
+    virtual HRESULT IUICommandHandlerExecute(UI_EXECUTIONVERB verb, const PROPERTYKEY* key, const PROPVARIANT* ppropvarValue, IUISimplePropertySet* pCommandExecutionProperties) override;
+};
