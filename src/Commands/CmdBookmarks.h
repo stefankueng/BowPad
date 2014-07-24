8c80b251a93174edd868ca226b7bbde8a9abcb69
gmisocpp <gmisocpp@d94fd7d9-36d9-44dd-b333-5229c786babd>
2014-07-24 05:07:01 +0000

    Move some code out of the header into a new .cpp file for interface clarity. Also reduce a dependency. No functionality change.

--- src/Commands/CmdBookmarks.h
+++ src/Commands/CmdBookmarks.h
@@ -18,135 +18,38 @@
 #pragma once
 #include "ICommand.h"
 #include "BowPadUI.h"
-#include "MRU.h"
-#include "ScintillaWnd.h"
 
+#include <vector>
+#include <tuple>
+
+namespace Scintilla
+{
+	struct SCNotification;
+};
 
 class CCmdBookmarks : public ICommand
 {
 public:
 
-    CCmdBookmarks(void * obj) : ICommand(obj)
-    {
-        int maxFiles = (int)CIniSettings::Instance().GetInt64(L"bookmarks", L"maxfiles", 30);
-        m_bookmarks.clear();
-        for (int i = 0; i < maxFiles; ++i)
-        {
-            std::wstring sKey = CStringUtils::Format(L"file%d", i);
-            std::wstring sBmData = CIniSettings::Instance().GetString(L"bookmarks", sKey.c_str(), L"");
-            if (!sBmData.empty())
-            {
-                std::vector<std::wstring> tokens;
-                stringtok(tokens, sBmData, true, L"*");
-                std::vector<long> lines;
-                std::wstring filepath;
-                int i = 0;
-                for (const auto& t : tokens)
-                {
-                    if (i == 0)
-                        filepath = t;
-                    else
-                        lines.push_back(_wtol(t.c_str()));
-                    ++i;
-                }
-                if (!filepath.empty() && !lines.empty())
-                    m_bookmarks.push_back(std::make_tuple(filepath, lines));
-            }
-        }
-    }
+    CCmdBookmarks(void * obj);
 
     ~CCmdBookmarks(void)
     {}
 
-
-    virtual bool Execute() override
+    bool Execute() override
     {
         return true;
     }
 
-    virtual UINT GetCmdId() override { return cmdBookmarks; }
+    UINT GetCmdId() override { return cmdBookmarks; }
 
-    virtual void OnDocumentClose(int index) override
-    {
-        CDocument doc = GetDocumentFromID(GetDocIDFromTabIndex(index));
-        if (doc.m_path.empty())
-            return;
-        bool bModified = false;
-        // look if the path is already in our list
-        for (auto it = m_bookmarks.cbegin(); it != m_bookmarks.cend(); ++it)
-        {
-            if (_wcsicmp(doc.m_path.c_str(), std::get<0>(*it).c_str()) == 0)
-            {
-                // remove the entry for this path
-                m_bookmarks.erase(it);
-                bModified = true;
-                break;
-            }
-        }
+    void OnDocumentClose(int index) override;
 
-        // find all bookmarks
-        std::vector<long> bookmarklines;
-        long line = -1;
-        do
-        {
-            line = (long)ScintillaCall(SCI_MARKERNEXT, line + 1, (1 << MARK_BOOKMARK));
-            if (line >= 0)
-                bookmarklines.push_back(line);
-        } while (line >= 0);
-
-        if (!bookmarklines.empty())
-        {
-            m_bookmarks.push_front(std::make_tuple(doc.m_path, bookmarklines));
-            bModified = true;
-        }
-
-        if (bModified)
-        {
-            // Save the bookmarks to the ini file
-            int maxFiles = (int)CIniSettings::Instance().GetInt64(L"bookmarks", L"maxfiles", 30);
-            int i = 0;
-            for (const auto& bm : m_bookmarks)
-            {
-                std::wstring sValue = std::get<0>(bm);
-                const auto lines = std::get<1>(bm);
-                for (const auto& line : lines)
-                {
-                    sValue += L"*";
-                    sValue += CStringUtils::Format(L"%ld", line);
-                }
-                std::wstring sKey = CStringUtils::Format(L"file%d", i);
-                CIniSettings::Instance().SetString(L"bookmarks", sKey.c_str(), sValue.c_str());
-                if (i > maxFiles)
-                    break;
-                ++i;
-            }
-        }
-    }
-
-    virtual void OnDocumentOpen(int index)
-    {
-        CDocument doc = GetDocumentFromID(GetDocIDFromTabIndex(index));
-        if (doc.m_path.empty())
-            return;
-
-        // look if the path is already in our list
-        for (auto it = m_bookmarks.cbegin(); it != m_bookmarks.cend(); ++it)
-        {
-            if (_wcsicmp(doc.m_path.c_str(), std::get<0>(*it).c_str()) == 0)
-            {
-                // apply the bookmarks
-                const auto lines = std::get<1>(*it);
-                for (const auto& line : lines)
-                {
-                    ScintillaCall(SCI_MARKERADD, line, MARK_BOOKMARK);
-                    DocScrollAddLineColor(DOCSCROLLTYPE_BOOKMARK, line, RGB(255, 0, 0));
-                }
-                break;
-            }
-        }
-    }
+    void OnDocumentOpen(int index) override;
 
 private:
+	// REVIEW: A case insensitive string map to vector<lomg>
+    // is a lot less code, way more readable and gets rid of tuple too!
     std::list<std::tuple<std::wstring, std::vector<long>>> m_bookmarks;
 };
 
@@ -158,33 +61,11 @@ public:
     {
     }
 
-    ~CCmdBookmarkToggle(void)
-    {
-    }
+    ~CCmdBookmarkToggle(void) { }
 
-    virtual bool Execute() override
-    {
-        long line = long(ScintillaCall(SCI_LINEFROMPOSITION, ScintillaCall(SCI_GETCURRENTPOS)));
+    bool Execute() override;
 
-        LRESULT state = ScintillaCall(SCI_MARKERGET, line);
-        if ((state & (1 << MARK_BOOKMARK)) != 0)
-        {
-            ScintillaCall(SCI_MARKERDELETE, line, MARK_BOOKMARK);
-            DocScrollRemoveLine(DOCSCROLLTYPE_BOOKMARK, line);
-        }
-        else
-        {
-            ScintillaCall(SCI_MARKERADD, line, MARK_BOOKMARK);
-            DocScrollAddLineColor(DOCSCROLLTYPE_BOOKMARK, line, RGB(255,0,0));
-        }
-        InvalidateUICommand(cmdBookmarkNext, UI_INVALIDATIONS_STATE, NULL);
-        InvalidateUICommand(cmdBookmarkPrev, UI_INVALIDATIONS_STATE, NULL);
-        InvalidateUICommand(cmdBookmarkClearAll, UI_INVALIDATIONS_STATE, NULL);
-
-        return true;
-    }
-
-    virtual UINT GetCmdId() override { return cmdBookmarkToggle; }
+    UINT GetCmdId() override { return cmdBookmarkToggle; }
 };
 
 class CCmdBookmarkClearAll : public ICommand
@@ -195,18 +76,11 @@ public:
     {
     }
 
-    ~CCmdBookmarkClearAll(void)
-    {
-    }
+    ~CCmdBookmarkClearAll(void) { }
 
-    virtual bool Execute() override
-    {
-        ScintillaCall(SCI_MARKERDELETEALL, MARK_BOOKMARK);
-        DocScrollClear(DOCSCROLLTYPE_BOOKMARK);
-        return true;
-    }
+    bool Execute() override;
 
-    virtual UINT GetCmdId() override { return cmdBookmarkClearAll; }
+    UINT GetCmdId() override { return cmdBookmarkClearAll; }
 };
 
 class CCmdBookmarkNext : public ICommand
@@ -218,46 +92,16 @@ public:
         InvalidateUICommand(UI_INVALIDATIONS_STATE, NULL);
     }
 
-    ~CCmdBookmarkNext(void)
-    {
-    }
+    ~CCmdBookmarkNext(void) { }
 
-    virtual bool Execute() override
-    {
-        long line = (long)ScintillaCall(SCI_LINEFROMPOSITION, ScintillaCall(SCI_GETCURRENTPOS));
-        line = (long)ScintillaCall(SCI_MARKERNEXT, line+1, (1 << MARK_BOOKMARK));
-        if (line >= 0)
-            ScintillaCall(SCI_GOTOLINE, line);
-        else
-        {
-            // retry from the start of the document
-            line = (long)ScintillaCall(SCI_MARKERNEXT, 0, (1 << MARK_BOOKMARK));
-            if (line >= 0)
-                ScintillaCall(SCI_GOTOLINE, line);
-        }
-        return true;
-    }
+    bool Execute() override;
 
-    virtual UINT GetCmdId() override { return cmdBookmarkNext; }
+    UINT GetCmdId() override { return cmdBookmarkNext; }
 
-    virtual void ScintillaNotify(Scintilla::SCNotification * pScn) override
-    {
-        if (pScn->nmhdr.code == SCN_MODIFIED)
-            InvalidateUICommand(UI_INVALIDATIONS_STATE, NULL);
-        else if ((pScn->nmhdr.code == SCN_MARGINCLICK) && (pScn->margin == SC_MARGE_SYBOLE) && !pScn->modifiers)
-            InvalidateUICommand(UI_INVALIDATIONS_STATE, NULL);
-    }
+    void ScintillaNotify(Scintilla::SCNotification * pScn) override;
 
-    virtual HRESULT IUICommandHandlerUpdateProperty(REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue) override
-    {
-        if (UI_PKEY_Enabled == key)
-        {
-            long nextpos = (long)ScintillaCall(SCI_MARKERNEXT, 0, (1 << MARK_BOOKMARK));
-            return UIInitPropertyFromBoolean(UI_PKEY_Enabled, nextpos >= 0, ppropvarNewValue);
-        }
-        return E_NOTIMPL;
-    }
-
+    HRESULT IUICommandHandlerUpdateProperty(
+		REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue) override;
 };
 
 class CCmdBookmarkPrev : public ICommand
@@ -269,44 +113,14 @@ public:
         InvalidateUICommand(UI_INVALIDATIONS_STATE, NULL);
     }
 
-    ~CCmdBookmarkPrev(void)
-    {
-    }
+    ~CCmdBookmarkPrev(void) { }
 
-    virtual bool Execute() override
-    {
-        long line = (long)ScintillaCall(SCI_LINEFROMPOSITION, ScintillaCall(SCI_GETCURRENTPOS));
-        line = (long)ScintillaCall(SCI_MARKERPREVIOUS, line-1, (1 << MARK_BOOKMARK));
-        if (line >= 0)
-            ScintillaCall(SCI_GOTOLINE, line);
-        else
-        {
-            // retry from the start of the document
-            line = (long)ScintillaCall(SCI_MARKERPREVIOUS, ScintillaCall(SCI_GETLINECOUNT), (1 << MARK_BOOKMARK));
-            if (line >= 0)
-                ScintillaCall(SCI_GOTOLINE, line);
-        }
-        return true;
-    }
+    bool Execute() override;
 
-    virtual UINT GetCmdId() override { return cmdBookmarkPrev; }
+    UINT GetCmdId() override { return cmdBookmarkPrev; }
 
-    virtual void ScintillaNotify(Scintilla::SCNotification * pScn) override
-    {
-        if (pScn->nmhdr.code == SCN_MODIFIED)
-            InvalidateUICommand(UI_INVALIDATIONS_STATE, NULL);
-        else if ((pScn->nmhdr.code == SCN_MARGINCLICK) && (pScn->margin == SC_MARGE_SYBOLE) && !pScn->modifiers)
-            InvalidateUICommand(UI_INVALIDATIONS_STATE, NULL);
-    }
+    void ScintillaNotify(Scintilla::SCNotification * pScn) override;
 
-    virtual HRESULT IUICommandHandlerUpdateProperty(REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue) override
-    {
-        if (UI_PKEY_Enabled == key)
-        {
-            long nextpos = (long)ScintillaCall(SCI_MARKERNEXT, 0, (1 << MARK_BOOKMARK));
-            return UIInitPropertyFromBoolean(UI_PKEY_Enabled, nextpos >= 0, ppropvarNewValue);
-        }
-        return E_NOTIMPL;
-    }
-
+    HRESULT IUICommandHandlerUpdateProperty(
+		REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue) override;
 };
