4c9dd134c1952b0bd06352e1405a0104fab55c38
gmisocpp <gmisocpp@d94fd7d9-36d9-44dd-b333-5229c786babd>
2014-08-01 07:30:40 +0000

    Simplify code by removing use of tuple.

--- src/Commands/CmdSession.cpp
+++ src/Commands/CmdSession.cpp
@@ -25,6 +25,11 @@
 #include "IniSettings.h"
 #include "CmdLineParser.h"
 
+namespace
+{
+    const int BP_MAX_SESSION_SIZE = 100;
+};
+
 bool CCmdSessionLoad::Execute()
 {
     RestoreSavedSession();
@@ -43,43 +48,45 @@ void CCmdSessionLoad::OnClose()
     int tabcount = GetTabCount();
     int activetab = GetActiveTabIndex();
     int saveindex = 0;
-    for (int i = 0; i < tabcount; ++i)
+    // No point saving more than we are prepared to load.
+    int savecount = min(tabcount, BP_MAX_SESSION_SIZE);
+    for (int i = 0; i < savecount; ++i)
     {
-        CDocument doc = GetDocumentFromID(GetDocIDFromTabIndex(i));
-        if (!doc.m_path.empty())
+        int docId = GetDocIDFromTabIndex(i);
+        CDocument doc = GetDocumentFromID(docId);
+        if (doc.m_path.empty())
+            continue;
+        if (i == activetab)
         {
-            if (i == activetab)
-            {
-                CPosData pos;
-                pos.m_nFirstVisibleLine   = ScintillaCall(SCI_GETFIRSTVISIBLELINE);
-                pos.m_nFirstVisibleLine   = ScintillaCall(SCI_DOCLINEFROMVISIBLE, pos.m_nFirstVisibleLine);
+            CPosData pos;
+            pos.m_nFirstVisibleLine   = ScintillaCall(SCI_GETFIRSTVISIBLELINE);
+            pos.m_nFirstVisibleLine   = ScintillaCall(SCI_DOCLINEFROMVISIBLE, pos.m_nFirstVisibleLine);
 
-                pos.m_nStartPos           = ScintillaCall(SCI_GETANCHOR);
-                pos.m_nEndPos             = ScintillaCall(SCI_GETCURRENTPOS);
-                pos.m_xOffset             = ScintillaCall(SCI_GETXOFFSET);
-                pos.m_nSelMode            = ScintillaCall(SCI_GETSELECTIONMODE);
-                pos.m_nScrollWidth        = ScintillaCall(SCI_GETSCROLLWIDTH);
+            pos.m_nStartPos           = ScintillaCall(SCI_GETANCHOR);
+            pos.m_nEndPos             = ScintillaCall(SCI_GETCURRENTPOS);
+            pos.m_xOffset             = ScintillaCall(SCI_GETXOFFSET);
+            pos.m_nSelMode            = ScintillaCall(SCI_GETSELECTIONMODE);
+            pos.m_nScrollWidth        = ScintillaCall(SCI_GETSCROLLWIDTH);
 
-                CIniSettings::Instance().SetString(L"TabSession", CStringUtils::Format(L"path%d", saveindex).c_str(), doc.m_path.c_str());
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"selmode%d", saveindex).c_str(), pos.m_nSelMode);
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"startpos%d", saveindex).c_str(), pos.m_nStartPos);
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"endpos%d", saveindex).c_str(), pos.m_nEndPos);
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"scrollwidth%d", saveindex).c_str(), pos.m_nScrollWidth);
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"xoffset%d", saveindex).c_str(), pos.m_xOffset);
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"firstvisible%d", saveindex).c_str(), pos.m_nFirstVisibleLine);
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"activetab%d", saveindex).c_str(), 1);
-            }
-            else
-            {
-                CIniSettings::Instance().SetString(L"TabSession", CStringUtils::Format(L"path%d", saveindex).c_str(), doc.m_path.c_str());
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"selmode%d", saveindex).c_str(), doc.m_position.m_nSelMode);
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"startpos%d", saveindex).c_str(), doc.m_position.m_nStartPos);
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"endpos%d", saveindex).c_str(), doc.m_position.m_nEndPos);
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"scrollwidth%d", saveindex).c_str(), doc.m_position.m_nScrollWidth);
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"xoffset%d", saveindex).c_str(), doc.m_position.m_xOffset);
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"firstvisible%d", saveindex).c_str(), doc.m_position.m_nFirstVisibleLine);
-                CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"activetab%d", saveindex).c_str(), 0);
-            }
+            CIniSettings::Instance().SetString(L"TabSession", CStringUtils::Format(L"path%d", saveindex).c_str(), doc.m_path.c_str());
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"selmode%d", saveindex).c_str(), pos.m_nSelMode);
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"startpos%d", saveindex).c_str(), pos.m_nStartPos);
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"endpos%d", saveindex).c_str(), pos.m_nEndPos);
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"scrollwidth%d", saveindex).c_str(), pos.m_nScrollWidth);
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"xoffset%d", saveindex).c_str(), pos.m_xOffset);
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"firstvisible%d", saveindex).c_str(), pos.m_nFirstVisibleLine);
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"activetab%d", saveindex).c_str(), 1);
+        }
+        else
+        {
+            CIniSettings::Instance().SetString(L"TabSession", CStringUtils::Format(L"path%d", saveindex).c_str(), doc.m_path.c_str());
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"selmode%d", saveindex).c_str(), doc.m_position.m_nSelMode);
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"startpos%d", saveindex).c_str(), doc.m_position.m_nStartPos);
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"endpos%d", saveindex).c_str(), doc.m_position.m_nEndPos);
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"scrollwidth%d", saveindex).c_str(), doc.m_position.m_nScrollWidth);
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"xoffset%d", saveindex).c_str(), doc.m_position.m_xOffset);
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"firstvisible%d", saveindex).c_str(), doc.m_position.m_nFirstVisibleLine);
+            CIniSettings::Instance().SetInt64 (L"TabSession", CStringUtils::Format(L"activetab%d", saveindex).c_str(), 0);
         }
         ++saveindex;
     }
@@ -87,8 +94,8 @@ void CCmdSessionLoad::OnClose()
 
 void CCmdSessionLoad::RestoreSavedSession()
 {
-    int activetab = -1;
-    for (int i = 0; i < 100; ++i)
+    int activeDoc = -1;
+    for (int i = 0; i < BP_MAX_SESSION_SIZE; ++i)
     {
         std::wstring key = CStringUtils::Format(L"path%d", i);
         std::wstring path = CIniSettings::Instance().GetString(L"TabSession", key.c_str(), L"");
@@ -99,35 +106,46 @@ void CCmdSessionLoad::RestoreSavedSession()
             if (HasActiveDocument())
             {
                 CDocument doc = GetActiveDocument();
-                doc.m_position.m_nSelMode           = (size_t)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"selmode%d", i).c_str(), 0);
-                doc.m_position.m_nStartPos          = (size_t)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"startpos%d", i).c_str(), 0);
-                doc.m_position.m_nEndPos            = (size_t)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"endpos%d", i).c_str(), 0);
-                doc.m_position.m_nScrollWidth       = (size_t)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"scrollwidth%d", i).c_str(), 0);
-                doc.m_position.m_xOffset            = (size_t)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"xoffset%d", i).c_str(), 0);
-                doc.m_position.m_nFirstVisibleLine  = (size_t)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"firstvisible%d", i).c_str(), 0);
-                SetDocument(GetDocIdOfCurrentTab(), doc);
+                auto& pos = doc.m_position;
+                pos.m_nSelMode           = (size_t)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"selmode%d", i).c_str(), 0);
+                pos.m_nStartPos          = (size_t)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"startpos%d", i).c_str(), 0);
+                pos.m_nEndPos            = (size_t)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"endpos%d", i).c_str(), 0);
+                pos.m_nScrollWidth       = (size_t)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"scrollwidth%d", i).c_str(), 0);
+                pos.m_xOffset            = (size_t)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"xoffset%d", i).c_str(), 0);
+                pos.m_nFirstVisibleLine  = (size_t)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"firstvisible%d", i).c_str(), 0);
+                int docId = GetDocIdOfCurrentTab();
+                APPVERIFY(docId >= 0);
+                SetDocument(docId, doc);
 
                 ScintillaCall(SCI_GOTOPOS, 0);
-                ScintillaCall(SCI_SETSELECTIONMODE, doc.m_position.m_nSelMode);
-                ScintillaCall(SCI_SETANCHOR, doc.m_position.m_nStartPos);
-                ScintillaCall(SCI_SETCURRENTPOS, doc.m_position.m_nEndPos);
+                ScintillaCall(SCI_SETSELECTIONMODE, pos.m_nSelMode);
+                ScintillaCall(SCI_SETANCHOR, pos.m_nStartPos);
+                ScintillaCall(SCI_SETCURRENTPOS, pos.m_nEndPos);
                 ScintillaCall(SCI_CANCEL);
                 if (ScintillaCall(SCI_GETWRAPMODE) != SC_WRAP_WORD)
                 {
                     // only offset if not wrapping, otherwise the offset isn't needed at all
-                    ScintillaCall(SCI_SETSCROLLWIDTH, doc.m_position.m_nScrollWidth);
-                    ScintillaCall(SCI_SETXOFFSET, doc.m_position.m_xOffset);
+                    ScintillaCall(SCI_SETSCROLLWIDTH, pos.m_nScrollWidth);
+                    ScintillaCall(SCI_SETXOFFSET, pos.m_xOffset);
                 }
                 ScintillaCall(SCI_CHOOSECARETX);
-                size_t lineToShow = ScintillaCall(SCI_VISIBLEFROMDOCLINE,doc.m_position.m_nFirstVisibleLine);
+                size_t lineToShow = ScintillaCall(SCI_VISIBLEFROMDOCLINE,pos.m_nFirstVisibleLine);
                 ScintillaCall(SCI_LINESCROLL, 0, lineToShow);
                 if ((int)CIniSettings::Instance().GetInt64(L"TabSession", CStringUtils::Format(L"activetab%d", i).c_str(), 0))
-                    activetab = GetActiveTabIndex();
+                {
+                    // Don't use the index to track the active tab, as it's probably
+                    // not safe long term to assume the index where a tab was loaded
+                    // remains the same after other files load.
+                    activeDoc = docId;
+                }
             }
         }
     }
-    if (activetab >= 0)
+    if (activeDoc >= 0)
+    {
+        int activetab = GetTabIndexFromDocID(activeDoc);
         TabActivateAt(activetab);
+    }
 }
 
 
@@ -171,25 +189,25 @@ bool CCmdSessionRestoreLast::Execute()
 {
     if (m_docstates.empty())
         return false;
-    // restore the tab that was closed last
-    auto pt = m_docstates.back();
-    if (OpenFile(std::get<0>(pt).c_str(), OpenFlags::AddToMRU))
+    const SessionItem si = m_docstates.back();
+    const auto& pos = si.posData;
+    if (OpenFile(si.path.c_str(), OpenFlags::AddToMRU))
     {
         ScintillaCall(SCI_GOTOPOS, 0);
 
-        ScintillaCall(SCI_SETSELECTIONMODE, std::get<1>(pt).m_nSelMode);
-        ScintillaCall(SCI_SETANCHOR, std::get<1>(pt).m_nStartPos);
-        ScintillaCall(SCI_SETCURRENTPOS, std::get<1>(pt).m_nEndPos);
+        ScintillaCall(SCI_SETSELECTIONMODE, pos.m_nSelMode);
+        ScintillaCall(SCI_SETANCHOR, pos.m_nStartPos);
+        ScintillaCall(SCI_SETCURRENTPOS, pos.m_nEndPos);
         ScintillaCall(SCI_CANCEL);
         if (ScintillaCall(SCI_GETWRAPMODE) != SC_WRAP_WORD)
         {
             // only offset if not wrapping, otherwise the offset isn't needed at all
-            ScintillaCall(SCI_SETSCROLLWIDTH, std::get<1>(pt).m_nScrollWidth);
-            ScintillaCall(SCI_SETXOFFSET, std::get<1>(pt).m_xOffset);
+            ScintillaCall(SCI_SETSCROLLWIDTH, pos.m_nScrollWidth);
+            ScintillaCall(SCI_SETXOFFSET, pos.m_xOffset);
         }
         ScintillaCall(SCI_CHOOSECARETX);
 
-        size_t lineToShow = ScintillaCall(SCI_VISIBLEFROMDOCLINE, std::get<1>(pt).m_nFirstVisibleLine);
+        size_t lineToShow = ScintillaCall(SCI_VISIBLEFROMDOCLINE, pos.m_nFirstVisibleLine);
         ScintillaCall(SCI_LINESCROLL, 0, lineToShow);
     }
     m_docstates.pop_back();
@@ -214,10 +232,10 @@ void CCmdSessionRestoreLast::OnDocumentClose(int index)
         pos.m_nSelMode            = ScintillaCall(SCI_GETSELECTIONMODE);
         pos.m_nScrollWidth        = ScintillaCall(SCI_GETSCROLLWIDTH);
 
-        m_docstates.push_back(std::make_tuple(doc.m_path, pos));
+        m_docstates.push_back(SessionItem(doc.m_path, pos));
     }
     else
     {
-        m_docstates.push_back(std::make_tuple(doc.m_path, doc.m_position));
+        m_docstates.push_back(SessionItem(doc.m_path, doc.m_position));
     }
 }
