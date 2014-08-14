e467515a01837ca0982a02284aaa4a68db53be1b
gmisocpp <gmisocpp@d94fd7d9-36d9-44dd-b333-5229c786babd>
2014-08-14 18:05:14 +0000

    Move more implementation from header into new CmdMisc.cpp

--- trunk/src/Commands/CmdMisc.h
+++ trunk/src/Commands/CmdMisc.h
@@ -17,11 +17,7 @@
 
 #pragma once
 #include "ICommand.h"
-#include "BowPad.h"
 #include "BowPadUI.h"
-#include "AppUtils.h"
-#include "Theme.h"
-#include "SysInfo.h"
 
 class CCmdDelete : public ICommand
 {
@@ -35,13 +31,13 @@ public:
     {
     }
 
-    virtual bool Execute() override
+    bool Execute() override
     {
         ScintillaCall(SCI_CLEAR);
         return true;
     }
 
-    virtual UINT GetCmdId() override { return cmdDelete; }
+    UINT GetCmdId() override { return cmdDelete; }
 };
 
 
@@ -57,13 +53,13 @@ public:
     {
     }
 
-    virtual bool Execute() override
+    bool Execute() override
     {
         ScintillaCall(SCI_SELECTALL);
         return true;
     }
 
-    virtual UINT GetCmdId() override { return cmdSelectAll; }
+    UINT GetCmdId() override { return cmdSelectAll; }
 };
 
 class CCmdGotoBrace : public ICommand
@@ -78,73 +74,27 @@ public:
     {
     }
 
-    virtual bool Execute() override
+    bool Execute() override
     {
         GotoBrace();
         return true;
     }
 
-    virtual UINT GetCmdId() override { return cmdGotoBrace; }
+    UINT GetCmdId() override { return cmdGotoBrace; }
 };
 
 class CCmdToggleTheme : public ICommand
 {
 public:
+    CCmdToggleTheme(void * obj);
+    ~CCmdToggleTheme();
 
-    CCmdToggleTheme(void * obj)
-        : ICommand(obj)
-        , text(0)
-        , back(0)
-        , high(0)
-    {
-        CTheme::Instance().GetRibbonColors(text, back, high);
-        int dark = (int)CIniSettings::Instance().GetInt64(L"View", L"darktheme", 0);
-        if (dark)
-        {
-            CTheme::Instance().SetDarkTheme(!CTheme::Instance().IsDarkTheme());
-            CTheme::Instance().SetRibbonColorsHSB(UI_HSB(0, 0, 255), UI_HSB(160, 0, 0), UI_HSB(160, 44, 0));
-        }
-        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_BooleanValue);
-    }
+    bool Execute() override;
 
-    ~CCmdToggleTheme(void)
-    {
-    }
+    UINT GetCmdId() override { return cmdToggleTheme; }
 
-    virtual bool Execute() override
-    {
-        if (!HasActiveDocument())
-            return false;
-        CTheme::Instance().SetDarkTheme(!CTheme::Instance().IsDarkTheme());
-        CDocument doc = GetActiveDocument();
-        SetupLexerForLang(doc.m_language);
-        if (CTheme::Instance().IsDarkTheme())
-        {
-            CTheme::Instance().SetRibbonColorsHSB(UI_HSB(0, 0, 255), UI_HSB(160, 0, 0), UI_HSB(160, 44, 0));
-        }
-        else
-            CTheme::Instance().SetRibbonColorsHSB(text, back, high);
+    HRESULT IUICommandHandlerUpdateProperty(REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue) override;
 
-        CIniSettings::Instance().SetInt64(L"View", L"darktheme", CTheme::Instance().IsDarkTheme() ? 1 : 0);
-
-        ScintillaCall(SCI_CLEARDOCUMENTSTYLE);
-        ScintillaCall(SCI_COLOURISE, 0, -1);
-        TabActivateAt(GetActiveTabIndex());
-
-        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_BooleanValue);
-        return true;
-    }
-
-    virtual UINT GetCmdId() override { return cmdToggleTheme; }
-
-    virtual HRESULT IUICommandHandlerUpdateProperty(REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue) override
-    {
-        if (UI_PKEY_BooleanValue == key)
-        {
-            return UIInitPropertyFromBoolean(UI_PKEY_BooleanValue, CTheme::Instance().IsDarkTheme(), ppropvarNewValue);
-        }
-        return E_NOTIMPL;
-    }
 private:
     UI_HSBCOLOR text;
     UI_HSBCOLOR back;
@@ -163,72 +113,22 @@ public:
     {
     }
 
-    virtual bool Execute() override
-    {
-        std::wstring userFile = CAppUtils::GetDataPath() + L"\\shortcuts.ini";
-        if (!PathFileExists(userFile.c_str()))
-        {
-            HRSRC hRes = FindResource(NULL, MAKEINTRESOURCE(IDR_SHORTCUTS), L"config");
-            if (hRes)
-            {
-                HGLOBAL hResourceLoaded = LoadResource(NULL, hRes);
-                if (hResourceLoaded)
-                {
-                    char * lpResLock = (char *) LockResource(hResourceLoaded);
-                    if (lpResLock)
-                    {
-                        char * lpStart = strstr(lpResLock, "#--");
-                        if (lpStart)
-                        {
-                            char * lpEnd = strstr(lpStart + 3, "#--");
-                            if (lpEnd)
-                            {
-                                HANDLE hFile = CreateFile(userFile.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-                                if (hFile != INVALID_HANDLE_VALUE)
-                                {
-                                    DWORD dwWritten = 0;
-                                    WriteFile(hFile, lpStart, (DWORD)(lpEnd-lpStart), &dwWritten, NULL);
-                                    CloseHandle(hFile);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return OpenFile(userFile.c_str(), 0);
-    }
+    bool Execute() override;
 
-    virtual UINT GetCmdId() override { return cmdConfigShortcuts; }
+    UINT GetCmdId() override { return cmdConfigShortcuts; }
 };
 
 class CCmdAutoBraces : public ICommand
 {
 public:
 
-    CCmdAutoBraces(void * obj) : ICommand(obj)
-    {
-        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_BooleanValue);
-    }
+    CCmdAutoBraces(void * obj);
 
-    ~CCmdAutoBraces(void)
-    {}
+    ~CCmdAutoBraces(void);
 
-    virtual bool Execute() override
-    {
-        CIniSettings::Instance().SetInt64(L"View", L"autobrace", CIniSettings::Instance().GetInt64(L"View", L"autobrace", 1) ? 0 : 1);
-        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_BooleanValue);
-        return true;
-    }
+    bool Execute() override;
 
-    virtual UINT GetCmdId() override { return cmdAutoBraces; }
+    UINT GetCmdId() override { return cmdAutoBraces; }
 
-    virtual HRESULT IUICommandHandlerUpdateProperty(REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue) override
-    {
-        if (UI_PKEY_BooleanValue == key)
-        {
-            return UIInitPropertyFromBoolean(UI_PKEY_BooleanValue, (BOOL)CIniSettings::Instance().GetInt64(L"View", L"autobrace", 1), ppropvarNewValue);
-        }
-        return E_NOTIMPL;
-    }
+    HRESULT IUICommandHandlerUpdateProperty(REFPROPERTYKEY key, const PROPVARIANT* /*ppropvarCurrentValue*/, PROPVARIANT* ppropvarNewValue) override;
 };
