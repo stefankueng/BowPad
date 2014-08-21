0cfec8c6dc0b86cd1793b9f9ba133491aaac7c65
gmisocpp <gmisocpp@d94fd7d9-36d9-44dd-b333-5229c786babd>
2014-08-21 12:16:09 +0000

    Remove virtual as already implied by override.

--- src/Commands/CmdStyleConfigurator.h
+++ src/Commands/CmdStyleConfigurator.h
@@ -34,8 +34,8 @@ protected:
     LRESULT CALLBACK        DlgFunc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
     LRESULT                 DoCommand(int id, int msg);
 
-    virtual bool            Execute() override { return true; }
-    virtual UINT            GetCmdId() override { return 0; }
+    bool            Execute() override { return true; }
+    UINT            GetCmdId() override { return 0; }
 
     static int CALLBACK     EnumFontFamExProc(const LOGFONT *lpelfe, const TEXTMETRIC *lpntme, DWORD FontType, LPARAM lParam);
     void                    SelectStyle( int style );
@@ -62,13 +62,13 @@ public:
         delete m_pStyleConfiguratorDlg;
     }
 
-    virtual bool Execute() override;
+    bool Execute() override;
 
-    virtual UINT GetCmdId() override { return cmdStyleConfigurator; }
+    UINT GetCmdId() override { return cmdStyleConfigurator; }
 
-    virtual void ScintillaNotify(Scintilla::SCNotification * pScn) override;
+    void ScintillaNotify(Scintilla::SCNotification * pScn) override;
 
-    virtual void TabNotify(TBHDR * ptbhdr) override;
+    void TabNotify(TBHDR * ptbhdr) override;
 
 private:
     CStyleConfiguratorDlg *           m_pStyleConfiguratorDlg;
