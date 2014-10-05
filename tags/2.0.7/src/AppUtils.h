9bad91854e5491f668c6ecfe62f9a67b87921ba6
steveking <steveking@d94fd7d9-36d9-44dd-b333-5229c786babd>
2014-10-05 13:45:19 +0000

    Create the tag for the 2.0.7 release.

---
+++ tags/2.0.7/src/AppUtils.h
@@ -0,0 +1,46 @@
+// This file is part of BowPad.
+//
+// Copyright (C) 2013-2014 - Stefan Kueng
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// See <http://www.gnu.org/licenses/> for a copy of the full license text
+//
+
+#pragma once
+#include <string>
+
+class CAppUtils
+{
+public:
+    CAppUtils(void);
+    ~CAppUtils(void);
+
+    static std::wstring             GetDataPath(HMODULE hMod = NULL);
+    static std::wstring             GetSessionID();
+    static bool                     CheckForUpdate(bool force);
+    static bool                     DownloadUpdate(HWND hWnd, bool bInstall);
+    static bool                     ShowUpdateAvailableDialog(HWND hWnd);
+    static HRESULT CALLBACK         TDLinkClickCallback(HWND hwnd, UINT uNotification, WPARAM wParam, LPARAM lParam, LONG_PTR dwRefData);
+    static bool                     HasSameMajorVersion(const std::wstring& path);
+    static HRESULT                  AddResStringItem(IUICollectionPtr& collection, int resId, int cat = UI_COLLECTION_INVALIDINDEX, IUIImage * pImg = nullptr);
+    static HRESULT                  AddStringItem(IUICollectionPtr& collection, LPCWSTR text, int cat = UI_COLLECTION_INVALIDINDEX, IUIImage * pImg = nullptr);
+    static HRESULT                  AddCategory(IUICollectionPtr& coll, int catId, int catNameResId);
+    static bool                     FailedShowMessage(HRESULT hr);
+    static bool                     ShowDropDownList(HWND hWnd, LPCWSTR ctrlName);
+    static HRESULT                  CreateImage(LPCWSTR resName, IUIImagePtr& pOutImg );
+    static bool                     HexStringToCOLORREF(const wchar_t* s, COLORREF* clr);
+    static bool                     TryParse(const wchar_t* s, int& result, bool emptyOk = false, int def = 0);
+private:
+    static std::wstring             updatefilename;
+    static std::wstring             updateurl;
+};
+
---
+++ tags/2.0.7/src/ScintillaWnd.h
@@ -0,0 +1,137 @@
+// This file is part of BowPad.
+//
+// Copyright (C) 2013-2014 - Stefan Kueng
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// See <http://www.gnu.org/licenses/> for a copy of the full license text
+//
+#pragma once
+#include "BaseWindow.h"
+#include "Scintilla.h"
+#include "DocScroll.h"
+
+#include <vector>
+
+class CPosData;
+
+#define INDIC_SELECTION_MARK        (INDIC_CONTAINER+1)
+#define INDIC_TAGMATCH              (INDIC_CONTAINER+2)
+#define INDIC_TAGATTR               (INDIC_CONTAINER+3)
+#define INDIC_FINDTEXT_MARK         (INDIC_CONTAINER+4)
+#define INDIC_URLHOTSPOT            (INDIC_CONTAINER+5)
+#define INDIC_URLHOTSPOTACTIVE      (INDIC_CONTAINER+6)
+
+const int SC_MARGE_LINENUMBER = 0;
+const int SC_MARGE_SYBOLE = 1;
+const int SC_MARGE_FOLDER = 2;
+
+const int MARK_BOOKMARK = 24;
+
+struct XmlMatchedTagsPos
+{
+    size_t tagOpenStart;
+    size_t tagNameEnd;
+    size_t tagOpenEnd;
+
+    size_t tagCloseStart;
+    size_t tagCloseEnd;
+};
+
+struct FindResult
+{
+    size_t start;
+    size_t end;
+    bool success;
+};
+
+class LexerData;
+
+class CScintillaWnd : public CWindow
+{
+public :
+    CScintillaWnd(HINSTANCE hInst)
+        : CWindow(hInst)
+        , m_pSciMsg(nullptr)
+        , m_pSciWndData(0)
+        , m_selTextMarkerCount(0)
+    {};
+    virtual ~CScintillaWnd() {}
+
+    bool Init(HINSTANCE hInst, HWND hParent);
+    bool InitScratch(HINSTANCE hInst);
+
+    sptr_t Call(unsigned int iMessage, uptr_t wParam = 0, sptr_t lParam = 0)
+    {
+        return m_pSciMsg(m_pSciWndData, iMessage, wParam, lParam);
+    }
+    sptr_t ConstCall(unsigned int iMessage, uptr_t wParam = 0, sptr_t lParam = 0) const
+    {
+        return m_pSciMsg(m_pSciWndData, iMessage, wParam, lParam);
+    }
+
+    void UpdateLineNumberWidth();
+    void SaveCurrentPos(CPosData * pPos);
+    void RestoreCurrentPos(const CPosData& pos);
+    void SetupLexerForExt(const std::wstring& ext);
+    void SetupLexerForLang(const std::wstring& lang);
+    void MarginClick(Scintilla::SCNotification * pNotification);
+    void MarkSelectedWord(bool clear);
+    void MatchBraces();
+    void GotoBrace();
+    void MatchTags();
+    bool GetSelectedCount(size_t& selByte, size_t& selLine);
+    void DocScrollClear(int type) { m_docScroll.Clear(type); }
+    void DocScrollAddLineColor(int type, size_t line, COLORREF clr) { m_docScroll.AddLineColor(type, line, clr); }
+    void DocScrollUpdate();
+    void DocScrollRemoveLine(int type, size_t line) { m_docScroll.RemoveLine(type, line); }
+    void MarkBookmarksInScrollbar();
+    void GotoLine(long line);
+    void Center(long posStart, long posEnd);
+    void SetTabSettings();
+    void SetEOLType(int eolType);
+    void AppendText(int len, const char* buf);
+    std::string GetLine(long line) const;
+    std::string GetTextRange(long startpos, long endpos) const;
+    size_t FindText(const std::string& tofind, long startpos, long endpos);
+    std::string GetSelectedText() const;
+    std::string GetCurrentLine() const;
+    std::string GetWordChars() const;
+    std::string GetWhitespaceChars() const;
+    long GetSelTextMarkerCount() const { return m_selTextMarkerCount; }
+
+    LRESULT CALLBACK HandleScrollbarCustomDraw( WPARAM wParam, NMCSBCUSTOMDRAW * pCustDraw );
+
+protected:
+    virtual LRESULT CALLBACK WinMsgHandler(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) override;
+
+    void SetupLexer(const LexerData& lexerdata, const std::map<int, std::string>& langdata);
+    void SetupDefaultStyles();
+
+    bool GetXmlMatchedTagsPos( XmlMatchedTagsPos& xmlTags );
+    FindResult FindText(const char *text, size_t start, size_t end, int flags);
+    FindResult FindOpenTag(const std::string& tagName, size_t start, size_t end);
+    size_t FindCloseAngle(size_t startPosition, size_t endPosition);
+    FindResult FindCloseTag(const std::string& tagName, size_t start, size_t end);
+    std::vector<std::pair<size_t, size_t>> GetAttributesPos(size_t start, size_t end);
+    bool IsXMLWhitespace(int ch) { return ch == ' ' || ch == '\t' || ch == '\r' || ch == '\n'; }
+    bool AutoBraces( WPARAM wParam );
+
+    void BookmarkAdd(long lineno);
+    void BookmarkDelete(int lineno);
+    bool IsBookmarkPresent(int lineno);
+    void BookmarkToggle(int lineno);
+private:
+    SciFnDirect                 m_pSciMsg;
+    sptr_t                      m_pSciWndData;
+    CDocScroll                  m_docScroll;
+    long                        m_selTextMarkerCount;
+};
