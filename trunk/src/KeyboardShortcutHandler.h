1b0d9f39d1543fbb65d64e5d3e086af23aacf02b
steveking <steveking@d94fd7d9-36d9-44dd-b333-5229c786babd>
2014-11-01 09:24:31 +0000

    Allow using the name of a plugin as the command name for a shortcut. (Closes issue #187) : Add shortcut names as shortcut ids

--- trunk/src/KeyboardShortcutHandler.h
+++ trunk/src/KeyboardShortcutHandler.h
@@ -1,6 +1,6 @@
 // This file is part of BowPad.
 //
-// Copyright (C) 2013 - Stefan Kueng
+// Copyright (C) 2013-2014 - Stefan Kueng
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -39,6 +39,7 @@ public:
     BOOL            fVirt2;
     WORD            key2;
     WORD            cmd;        // the command
+    std::wstring    sCmd;       // the command as a string
 };
 
 class CKeyboardShortcutHandler
@@ -54,6 +55,7 @@ public:
     const std::map<std::wstring, int>&      GetResourceData() const { return m_resourceData; }
     const std::map<std::wstring, UINT>&     GetVirtKeys() const { return m_virtkeys; }
     const std::vector<KSH_Accel>&           GetAccelerators() const { return m_accelerators; }
+    void                                    AddCommand(const std::wstring& name, int id);
 
 private:
     CKeyboardShortcutHandler(void);
