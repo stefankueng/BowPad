3875512fc9c7f0ad819500abe73da4f2160a2f3e
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2015-10-28 20:19:00 +0000

    Handle the lexer changed event and rescan the functions.

--- src/Commands/CmdFunctions.cpp
+++ src/Commands/CmdFunctions.cpp
@@ -1,6 +1,6 @@
 // This file is part of BowPad.
 //
-// Copyright (C) 2013-2014 - Stefan Kueng
+// Copyright (C) 2013-2015 - Stefan Kueng
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -401,6 +401,16 @@ void CCmdFunctions::OnDocumentSave(int index, bool bSaveAs)
 {
     if (bSaveAs)
         ScheduleFunctionUpdate(GetDocIDFromTabIndex(index), FunctionUpdateReason::DocSave);
+}
+
+void CCmdFunctions::OnLexerChanged(int /*lexer*/)
+{
+    auto docId = GetDocIdOfCurrentTab();
+    if (docId >= 0)
+    {
+        ScheduleFunctionUpdate(docId, FunctionUpdateReason::DocModified);
+        InvalidateFunctionsEnabled();
+    }
 }
 
 void CCmdFunctions::UpdateFunctions( bool bForce )
--- src/Commands/CmdFunctions.h
+++ src/Commands/CmdFunctions.h
@@ -1,6 +1,6 @@
 // This file is part of BowPad.
 //
-// Copyright (C) 2013-2014 - Stefan Kueng
+// Copyright (C) 2013-2015 - Stefan Kueng
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -91,6 +91,7 @@ public:
 
     virtual void OnDocumentSave(int index, bool bSaveAs) override;
 
+    virtual void OnLexerChanged(int lexer) override;
 private:
 
     void FindFunctions(int docID, bool bForce);
