feeaf1c73db702a0ab50388e65580a3c58cf2e70
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2016-02-06 08:32:58 +0000

    Show "ANSI" for the local codepage.

--- src/Document.cpp
+++ src/Document.cpp
@@ -1,6 +1,6 @@
 // This file is part of BowPad.
 //
-// Copyright (C) 2013-2014 - Stefan Kueng
+// Copyright (C) 2013-2014, 2016 - Stefan Kueng
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -65,7 +65,10 @@ std::wstring CDocument::GetEncodingString()
         sEnc = L"ANSI";
         break;
     default:
-        sEnc = CStringUtils::Format(L"codepage: %d", m_encoding);
+        if (m_encoding == GetACP())
+            sEnc = L"ANSI";
+        else
+            sEnc = CStringUtils::Format(L"codepage: %d", m_encoding);
         break;
     }
     if (m_bHasBOM)
