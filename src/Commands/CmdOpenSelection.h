2c294fd5b849a030150eab1f179b96f23a62203c
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2016-05-02 18:47:59 +0000

    invalidate the label and enabled state immediately in the constructor: if it's not invalidated here, the first attempt to show the context menu will only show the pre-set text for the command label.

--- src/Commands/CmdOpenSelection.h
+++ src/Commands/CmdOpenSelection.h
@@ -1,6 +1,6 @@
 // This file is part of BowPad.
 //
-// Copyright (C) 2014 - 2016 Stefan Kueng
+// Copyright (C) 2014-2016 Stefan Kueng
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -23,9 +23,7 @@ class CCmdOpenSelection : public ICommand
 {
 public:
 
-    CCmdOpenSelection(void * obj) : ICommand(obj)
-    {
-    }
+    CCmdOpenSelection(void * obj);
 
     ~CCmdOpenSelection()
     {
