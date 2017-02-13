45fe80cee6b0f950d4adc601860d3807a646c177
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2017-02-13 19:12:31 +0000

    Make UI-changing commands execute after the main initialization is done, not in the constructor.

--- src/Commands/CmdVerticalEdge.h
+++ src/Commands/CmdVerticalEdge.h
@@ -1,6 +1,6 @@
 // This file is part of BowPad.
 //
-// Copyright (C) 2013-2014, 2016 - Stefan Kueng
+// Copyright (C) 2013-2014, 2016-2017 - Stefan Kueng
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -25,16 +25,19 @@ public:
 
     CCmdVerticalEdge(void * obj) : ICommand(obj)
     {
-        int ve = (int)CIniSettings::Instance().GetInt64(L"View", L"verticaledge", 0);
-        ScintillaCall(SCI_SETEDGECOLUMN, ve);
-        ScintillaCall(SCI_SETEDGEMODE, ve ? EDGE_LINE : EDGE_NONE);
-        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_DecimalValue);
     }
 
     ~CCmdVerticalEdge()
     {
     }
 
+    void AfterInit() override
+    {
+        int ve = (int)CIniSettings::Instance().GetInt64(L"View", L"verticaledge", 0);
+        ScintillaCall(SCI_SETEDGECOLUMN, ve);
+        ScintillaCall(SCI_SETEDGEMODE, ve ? EDGE_LINE : EDGE_NONE);
+        InvalidateUICommand(UI_INVALIDATIONS_PROPERTY, &UI_PKEY_DecimalValue);
+    }
     bool Execute() override { return true; }
 
     UINT GetCmdId() override { return cmdVerticalEdge; }
