02a9adbdfe659b8dff386582dff2d011efa70787
Stefan Küng <stefankueng@gmail.com>
2020-08-20 16:07:41 +0200

    change the name of the backup files to avoid name-collisions. Part of #73

--- src/Commands/CmdSession.cpp
+++ src/Commands/CmdSession.cpp
@@ -133,7 +133,7 @@ void CCmdSessionLoad::OnClose()
             {
                 filename = title;
             }
-            auto backupPath = CStringUtils::Format(L"%s\\%s.%d", sessionPath.c_str(), filename.c_str(), saveindex);
+            auto backupPath = CStringUtils::Format(L"%s\\%d%s.%d", sessionPath.c_str(), saveindex, filename.c_str(), saveindex);
             settings.SetString(g_sessionSection, CStringUtils::Format(L"origpath%d", saveindex).c_str(), doc.m_path.c_str());
             settings.SetString(g_sessionSection, CStringUtils::Format(L"origtitle%d", saveindex).c_str(), title.c_str());
             doc.m_path = backupPath;
