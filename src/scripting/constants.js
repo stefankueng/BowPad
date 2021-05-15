a5a7398bdc024c4aafaa603a658ad86dd4314a05
Stefan Küng <stefankueng@gmail.com>
2021-05-15 10:42:39 +0200

    add constants file to help with plugin development

---
+++ src/scripting/constants.js
@@ -0,0 +1,14 @@
+// constants you can use in your plugin
+
+
+// constants for the 'code' parameter in the TabNotify callback
+var TCN_SELCHANGE         = -551  // the active tab has changed
+var TCN_SELCHANGING       = -552  // the active tab is about to change
+var TCN_TABDROPPED        = 0x401 // tab is dropped to the active BP window
+var TCN_TABDROPPEDOUTSIDE = 0x402 // tab is dropped outsife of BP
+var TCN_TABDELETE         = 0x403 // a tab was deleted
+var TCN_ORDERCHANGED      = 0x405 // the tab order was changed
+var TCN_REFRESH           = 0x406 // tab is refreshed/reloaded
+var TCN_GETCOLOR          = 0x407 // BP asks for the tab color
+
+
